1b05786e1b5e767bc40810239d407159cbf62add
moisha <mikhail.yukhovich@gmail.com>
2025-08-20 15:52:00 +0300

    Initial commit

---
+++ DjangoREST/mysite/myapi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Hero(models.Model):
+    name = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
