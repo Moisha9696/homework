79f33a966a0fa27cabed17b1dc69ad23546d3487
moisha <mikhail.yukhovich@gmail.com>
2025-08-20 16:18:12 +0300

    Изменение в модели

--- DjangoREST/mysite/myapi/models.py
+++ DjangoREST/mysite/myapi/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Hero(models.Model):
     name = models.CharField(max_length=50)
-    alias = models.CharField(max_length=50)
+    alias = models.CharField(max_length=40)
     age = models.IntegerField(default=0)
 
     def __str__(self):
